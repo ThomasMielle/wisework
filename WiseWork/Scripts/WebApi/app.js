9fd5c5015dd8578b036920f9d2ff70c8aef92f4c
founet <aobmilan@gmail.com>
2015-04-28 12:48:13 +0200

    0.19.3
    La création de RDV focntionne

--- WiseWork/Scripts/WebApi/app.js
+++ WiseWork/Scripts/WebApi/app.js
@@ -114,6 +114,44 @@ WiseWorkController.controller('salonController', ['$scope', '$http', '$routePara
             .error(function (data, status) {
                 $scope.status = status;
             });
+
+        $scope.newMessage = "/rdv 12/05/2015 14:05:00 15:00:00 'Salon' ";
+        var CreateRdvPath = "/api/Salon/createRdv";
+        $scope.envoyerMessage = function () {
+            var pattern = new RegExp("/rdv");
+
+            if (pattern.test($scope.newMessage)) {
+                var str = $scope.newMessage.split("/rdv");
+                var eventParams = str[1].split(" ");
+                var dateDebut = eventParams[1];
+                var heureDebut = eventParams[2];
+                var heureFin = eventParams[3];
+                var titreRdv = eventParams[4];
+                var event = {
+                    dateDebut: dateDebut,
+                    heureDebut: heureDebut,
+                    heureFin: heureFin,
+                    titreRdv: titreRdv,
+                    user: { email: "aobmilan@gmail.com", pwd: "pwdddd=" },
+                    invites: [
+                        { email: "bah.founet@gmail.com" },
+                        { email: "aobmilan@yahoo.fr" },
+                        { email: "aobmilan@facebook.com" },
+                    ]
+
+                }
+                $http.post(CreateRdvPath, event
+                    )
+                    .success(function (data) {
+                        alert("Rendez vous crée avec succès");
+                    })
+                    .error(function (data) {
+                        alert("Erreur : Le rdv n'a pas été crée ");
+                    });
+
+            }
+
+        };
     }
 ]);
 
